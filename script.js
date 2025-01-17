6df905326717cd37e63d6fd8423f28e9e6c6c9a2
twmcdunn <twcdunn@gmail.com>
2025-01-17 13:03:34 -0600

    asdfasf

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-document.body.appendChild(document.createTextNode("HI FROM A TEXT NODE"));
+document.body.appendChild(document.createTextNode("HI FROM A TEXT NODE1"));
 
 window.addEventListener("devicemotion", (event) => {
-    console.log(`${event.acceleration.x} m/s2`);
-    document.body.innerText = "ACCEL: " + `${event.acceleration.x} m/s2`
-  });
+  console.log(`${event.acceleration.x} m/s2`);
+  document.body.innerText = "ACCEL: " + `${event.acceleration.x} m/s2`;
+});
 
-  //blah
-  
+
+window.ondevicemotion = (event) => {
+  // Process event.acceleration, event.accelerationIncludingGravity,
+  // event.rotationRate and event.interval
+  console.log(`${event.acceleration.x} m/s2`);
+  document.body.innerText = "ON MOTION ACCEL: " + `${event.acceleration.x} m/s2`;
+  document.body.appendChild(document.createTextNode("ON MOTION ACCEL: " + `${event.acceleration.x} m/s2`));
+
+};
