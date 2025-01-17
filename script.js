9b7e9f975c5e4ba6f33e17146267b3704ccb73b5
twmcdunn <twcdunn@gmail.com>
2025-01-17 12:40:54 -0600

    added scripts

---
+++ script.js
@@ -0,0 +1,6 @@
+
+
+window.addEventListener("devicemotion", (event) => {
+    console.log(`${event.acceleration.x} m/s2`);
+    document.body.innerText = "ACCEL: " + `${event.acceleration.x} m/s2`
+  });
